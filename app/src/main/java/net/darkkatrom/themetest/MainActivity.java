65eb0be284a5c31288d456fc872b951756e5f2e1
XXMrHyde <xxmrhyde@gmx.co.uk>
2019-09-26 14:22:32 +0200

    Add app theming:
    - Add 3 buttons:
      - Light theme
      - Dark theme
      - System theme
    
    This is only a base implementation.
    
    TO-DO:
    - Add settings
    - Move app theming to settings
    - Only show needed settings,
      (no need for dark theme, when the system theme is set to dark)
    - Show info text about current app- and system theme

--- app/src/main/java/net/darkkatrom/themetest/MainActivity.java
+++ app/src/main/java/net/darkkatrom/themetest/MainActivity.java
@@ -1,6 +1,9 @@
 package net.darkkatrom.themetest;
 
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void lightTheme(View v) {
+        updateTheme(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    public void darkTheme(View v) {
+        updateTheme(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    public void systemTheme(View v) {
+        updateTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+    }
+
+    public void updateTheme(int theme) {
+        AppCompatDelegate.setDefaultNightMode(theme);
     }
 }
